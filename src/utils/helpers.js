b2cc2d47a6bb2883c265018a20e147ea34647470
Valeriy <valeriy.sorokobatko@gmail.com>
2015-06-16 13:30:10 +0300

    remove typo

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
 import path  from 'path';
 
-export var toUnifiedPath = path => path.replace(/[\\\/]+/g, '/');git
+export var toUnifiedPath = path => path.replace(/[\\\/]+/g, '/');
 export var sep = path.sep === '/' ? '\\x2f' : '\\x5c';
